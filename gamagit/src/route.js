e83a2067d4c1ec8d33e71d92e2c8e206f3a573ba
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-11 23:41:07 -0300

    routes

--- gamagit/src/route.js
+++ gamagit/src/route.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Home from "./home";
 import Repositories from "./repositories";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route patch="/repositories" component={Repositories} />
+        <Route path="/" exact component={Home} />
+        <Route path="/repositories" component={Repositories} />
       </Switch>
     </BrowserRouter>
   );
