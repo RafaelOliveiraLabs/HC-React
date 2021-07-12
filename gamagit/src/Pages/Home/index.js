d89148c667d81d92935f4ef91829f1cd17dee3c2
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-12 12:54:30 -0300

    conditionalRendering

--- gamagit/src/Pages/Home/index.js
+++ gamagit/src/Pages/Home/index.js
@@ -6,26 +6,36 @@ import { useHistory } from "react-router-dom";
 function Home() {
   const history = useHistory();
   const [usuario, setUsuario] = useState("");
+  const [erro, setErro] = useState(false);
 
   function handlePesquisa() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then((response) => {
-      const repositories = response.data;
-      const repositoriesName = [];
+    axios
+      .get(`https://api.github.com/users/${usuario}/repos`)
+      .then((response) => {
+        const repositories = response.data;
+        const repositoriesName = [];
 
-      repositories.map((repository) => {
-        repositoriesName.push(repository.name);
+        repositories.map((repository) => {
+          repositoriesName.push(repository.name);
+        });
+        localStorage.setItem("repositoriesName", JSON.stringify(repositoriesName));
+        history.push("/Repositories");
+      })
+      .catch((err) => {
+        setErro(true);
       });
-      localStorage.setItem("repositoriesName", JSON.stringify(repositoriesName));
-      history.push("/Repositories");
-    });
   }
 
   return (
     <S.Container>
-      <S.Input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
-      <S.Button type="button" onClick={handlePesquisa}>
-        Pesquisar
-      </S.Button>
+      <S.Content>
+        <S.Input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+        <S.Button type="button" onClick={handlePesquisa}>
+          Pesquisar
+        </S.Button>
+      </S.Content>
+
+      {erro ? <S.ErrorMsg>Usuário não encontrado</S.ErrorMsg> : ""}
     </S.Container>
   );
 }
